5cfc15099f218c8534a76b6708d11381d47f5e51
Shakyamuni177te <peter.carter.mail@gmail.com>
2015-04-24 21:59:05 +0100

    deleted middleware function for db

--- app.js
+++ app.js
@@ -28,13 +28,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Make database accessable to router
-
-app.use(function(req, res, next){
-		req.db = db;
-		next();
-})
-
 app.use('/', routes);
 app.use('/users', users);
 
